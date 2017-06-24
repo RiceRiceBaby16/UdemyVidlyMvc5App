d4078a58501adfae80b9d51437269c98a9a58153
Andy Brookes <andrew.n.brookes@gmail.com>
2017-06-24 17:55:51 +0100

    Add Name to MembershipType and display in View

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -5,6 +5,7 @@
         public byte DiscountRate { get; set; }
         public byte DurationInMonths { get; set; }
         public byte Id { get; set; }
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
     }
 }
