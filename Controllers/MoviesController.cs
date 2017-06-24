b9d5edcff94441567c47148bbb7f2e7ccced6233
Andy Brookes <andrew.n.brookes@gmail.com>
2017-06-24 19:48:31 +0100

    Add Movies table and changes MoviesController to reference Movies DbSet.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,71 +1,38 @@
-﻿using CourseByMosh.Models;
+﻿#region Namespaces
+
+using CourseByMosh.Models;
 using CourseByMosh.ViewModels;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+
+#endregion Namespaces
 
 namespace CourseByMosh.Controllers
 {
     public class MoviesController : Controller
     {
-        private List<Movie> _movies;
+        private ApplicationDbContext _context;
 
         public MoviesController()
         {
-            _movies = new List<Movie>
-            {
-                //new Movie{Name = "Shrek"},
-                //new Movie{Name = "Wall-e"}
-            };
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_context != null)
+                _context.Dispose();
         }
 
         public ActionResult Index()
         {
-            var moviesViewModel = new MoviesViewModel { Movies = _movies };
-            return View(moviesViewModel);
+            return View(new MoviesViewModel { Movies = GetMovies() });
         }
 
-        // GET: Movies
-        public ActionResult Random()
+        private List<Movie> GetMovies()
         {
-            var movie = new Movie { Name = "Shrek!" };
-            //ViewData["Movie"] = movie;//dynamic = pain!
-            //ViewBag.Movie = movie;//dynamic = pain!
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2" }
-            };
-            var viewModel = new RandomMovieViewModel
-            {
-                Customers = customers,
-                Movie = movie
-            };
-            return View(viewModel);// strongly-typed = Yay! :-)
-
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
-            //return HttpNotFound();
-            //return new ContentResult("Hi"); => return Content();
-            //return new PartialViewResult(); => return PartialView();
-            //return new RedirectResult("url"); => return RedirectResult("url");
-            //return new RedirectToRouteResult("routeName", new System.Web.Routing.RouteValueDictionary(new Dictionary<string, object>())); =>
-            //return RedirectToRoute(...);
-            //return new JsonResult(); => return Json();
-            //return new FileResult(); => return File();
-            //return new HttpNotFoundResult(); => return HttpNotFound();
-            //return new EmptyResult();
-            //return new ViewResult(); => View
-            //return View(new Movie { Name = "Shrek!" });
-        }
-
-        // The below is attribute routing
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]// define constraints.
-        //*** OTHER CONSTRAINTS: min, max, minlength, maxlength, int, float, guid
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-            return Content(year + "/" + month);
+            return _context.Movies.ToList();
         }
     }
 }
--- ViewModels/MoviesViewModel.cs
+++ ViewModels/MoviesViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using CourseByMosh.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CourseByMosh.ViewModels
 {
