e0c93facb2c60a8ade2161dbaf556b894fcaffc6
Andy Brookes <andrew.n.brookes@gmail.com>
2017-06-25 17:55:51 +0100

    Initial Customers API implementation.

---
+++ Controllers/API/CustomersController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using CourseByMosh.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace CourseByMosh.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/customers/id
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)// could name PostCustomer, which, by convention, would do away with the HttpPost attribute
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+
+            var existingCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (existingCustomer == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+            Mapper.Initialize(config => config.CreateMap<Customer, Customer>());
+            Mapper.Map(customer, existingCustomer);
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var existingCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (existingCustomer == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(existingCustomer);
+            _context.SaveChanges();
+        }
+    }
+}
